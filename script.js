556a5281db9c96457f26f14735ef832f9e19dafe
JerryGLima <133042786+JerryGLima@users.noreply.github.com>
2026-04-20 11:51:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ const btnAbrir = document.getElementById('btn-abrir');
 const musica = document.getElementById('musica');
 const overlay = document.getElementById('overlay');
 
+// Clique para entrar
 btnAbrir.addEventListener('click', () => {
+    // Esconde tela de entrada
     overlay.style.transition = 'opacity 0.8s ease, visibility 0.8s';
     overlay.style.opacity = '0';
     overlay.style.visibility = 'hidden';
 
+    // Inicia música
     musica.volume = 0.5;
     musica.play().catch(e => console.log("Áudio bloqueado"));
 
+    // Ativa animações de entrada do conteúdo
     setTimeout(checkReveal, 300);
 });
 
+// Lógica de Scroll Reveal
 function checkReveal() {
     const reveals = document.querySelectorAll('.reveal');
     reveals.forEach(element => {
@@ -27,6 +32,7 @@ function checkReveal() {
 window.addEventListener('scroll', checkReveal);
 window.onload = checkReveal;
 
+// Garante que comece no topo
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 };
